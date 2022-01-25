37520af134496a92f405807b74f756b174419c94
Nagesh_Maddala <nagesh_maddala@dell.com>
2022-01-24 21:18:11 -0600

    moved the middleware logic code to controllers

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,9 @@
 //express router
 const express = require("express");
 const multer = require("multer");
-
-const Post = require('../models/post');
+const PostController = require("../controllers/posts");
 const checkAuth = require('../middleware/check-auth');
-
 const router = express.Router();
-
 const MIME_TYPE_MAP = {
   'image/png': 'png',
   'image/jpeg': 'jpg',
@@ -31,7 +28,6 @@ const storage = multer.diskStorage({
   }
 });
 
-
 //add middleware
 // app.use((req, res, next) => {
 //   console.log('First middleware');
@@ -49,158 +45,31 @@ const storage = multer.diskStorage({
 //app.delete() and so on..
 //multer package is used to extract the file content
 //this route must be protected
-router.post("", checkAuth, multer({ storage: storage }).single("image"), (req, res, next) => {
-  // const post = req.body;
-  // console.log(post);
-  // protocol gives us http or https
-  const url = req.protocol + '://' + req.get("host");
-
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
-
-  // console.log(req.userData);
-  //spot test from returning value from here
-  // return res.status(200).json({});
-  // console.log(post);
-  post.save().then(createdPost => {
-    console.log("successfully added new record at the database");
-    res.status(201).json({
-      message: 'Post added successfully',
-      //postId: result._id
-      post: {
-        ...createdPost,
-        id: createdPost._id,
-        // title: createdPost.title,
-        // content: createdPost.content,
-        // imagePath: createdPost.imagePath
-      }
-    });
-  })
-    .catch(error => {
-      res.status(500).json({
-        message: "Creating a post failed"
-      })
-    });
-
-  //still need to return 201 saying everything is good
-  // res.status(201).json({
-  //   message: 'Post added successfully'
-  // });
-
-  //to extract the payload coming from the request
-  //install the package - body-parser which makes it easy
-  //to extract the required data from the incoming request
-  //npm install --save body-parser
-  //it adds specialy property to incoming request
-
-});
+router.post("",
+  checkAuth,
+  multer({ storage: storage }).single("image"),
+  PostController.createPost);
 
 // basically it honors only the requests if the path
 // contains '/api/posts' and discards the other requests
 // app.use is similar to app.get middleware
 // app.use('/api/posts', (req, res, next) => {
 // https://mongoosejs.com/docs/guide.html#query-helpers
-router.get('', (req, res, next) => {
-  //res.send('Hello from express!');
-
-  //dummy data
-  // const posts = [
-  //   {
-  //     id: 'id1',
-  //     title: 'First server-side post',
-  //     content: 'This is coming from the server'
-  //   },
-  //   {
-  //     id: 'id2',
-  //     title: 'Second server-side post',
-  //     content: 'This is coming from the server'
-  //   }
-  // ];
-
-  Post.find()
-    .then(documents => {
-      console.log(documents);
-      res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: documents
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Fetching posts failed!"
-      });
-    });
-
-  //to send response in json format
-  //set the optional status and don't call next unless you want to pass
-  //it to next middleware
-  // res.status(200).json({
-  //   message: 'Posts fetched successfully',
-  //   posts: posts
-  // });
-});
+router.get('', PostController.getPosts);
 
 //:id dynamic path segment
 // id is passed from the server
 // this route must be protected
-router.delete("/:id", checkAuth, (req, res, next) => {
-  console.log(req.params.id);
-  Post.deleteOne({
-    _id: req.params.id,
-    creator: req.userData.userId
-  }).then(result => {
-    console.log(result);
-    if (result.deletedCount > 0) {
-      res.status(200).json({ message: 'Deletion successful!' });
-    } else {
-      res.status(401).json({ message: 'Not Authorized!' });
-    }
-  })
-});
+router.delete("/:id", checkAuth, PostController.deletePost);
 
 //this route must be protected
-router.put("/:id", checkAuth, multer({ storage: storage }).single("image"),
-  (req, res, next) => {
+router.put("/:id",
+  checkAuth,
+  multer({ storage: storage }).single("image"),
+  PostController.updatePost
+);
 
-    //not worried about the edit logic for now..
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      creator: req.userData.userId
-    })
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-      //console.log(result);
-      if (result.modifiedCount > 0) {
-        res.status(200).json({ message: 'Update successful!' });
-      } else {
-        res.status(401).json({ message: 'Not Authorized!' });
-      }
-    })
-      .catch(error => {
-        res.status(500).json({
-          message: "Couldn't update post!"
-        });
-      });
-  });
-
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'Post not found!' });
-    }
-  }).catch(error => {
-    res.status(500).json({
-      message: "Fetching post failed!"
-    });
-  });
-});
+router.get("/:id", PostController.getPost);
 
 //node module syntax to export the module
 //set the whole router object
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,83 +1,14 @@
 //code for signup and login routes backend code
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-const user = require("../models/user");
-
 const router = express.Router();
+const UserController = require("../controllers/user");
 
 //Note: don't save the raw password for the security reasons
 // encrypt or hash it before saving the password to the database
 // npm install --save bcrypt
 // bcrypt packages does that for us
 
-router.post("/signup", (req, res, next) => {
-  //hash the password
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(result => {
-          res.status(201).json({
-            message: 'User created!',
-            result: result
-          });
-        })
-        .catch(err => {
-          // res.status(500).json({
-          //   error: err
-          // });
-          // we can customize the error object returned back to client
-          res.status(500).json({
-            message: "Invalid authentication credentials!"
-          });
-        });
-    });
-});
-
-router.post("/login", (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email }).then(user => {
-    if (!user) {
-      console.log(user);
-      return res.status(401).json({
-        message: "Auth failed"
-      });
-    }
-    fetchedUser = user;
-    //found user with email address
-    //now compare the pwd
-    return bcrypt.compare(req.body.password, user.password);
-  }).then(result => {
-    if (!result) {
-      return res.status(401).json({
-        message: "Auth failed"
-      });
-    }
-    //valid pwd
-    //create json webtoken
-    //jwt.io read more about that
-    //below line creates token
-    const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id }, 'secret_this_should_be_longer',
-      { expiresIn: "1h" });
-    res.status(200).json({
-      token: token,
-      // 3600 seconds
-      expiresIn: 3600,
-      userId: fetchedUser._id
-    });
-
-  }).catch(err => {
-    console.log(err);
-    return res.status(401).json({
-      message: "Invalid authentication credentials!"
-    });
-  });
-});
-
+router.post("/signup", UserController.createUser);
+router.post("/login", UserController.userLogin);
 
 module.exports = router;
